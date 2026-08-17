a83659ff31e9d0eb580fc616b6a17c27a16594c1
Halls499 <raul.mazzone@etec.sp.gov.br>
2026-08-16 21:35:53 -0300

    Correção para ficaro ícone certo

--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// 🚀 Adiciona o registro do Service Worker para o PWA funcionar perfeitamente
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((reg) => console.log('Service Worker registrado com sucesso!', reg))
+      .catch((err) => console.log('Falha ao registrar o Service Worker:', err));
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
