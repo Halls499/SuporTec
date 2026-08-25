35c1a18c8d4e4b39c42b50f2761b27fe88a17d9f
Halls499 <raul.mazzone@etec.sp.gov.br>
2026-08-24 22:47:35 -0300

    acaba pelo amor de Deus

--- Back/config/database.js
+++ Back/config/database.js
@@ -4,16 +4,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 console.log("🔍 DEBUG VARIÁVEIS:");
-console.log("HOST:", process.env.DB_HOST);
-console.log("USER:", process.env.DB_USER);
-console.log("PORT:", process.env.DB_PORT);
+console.log("HOST:", process.env.MYSQLHOST || process.env.DB_HOST);
+console.log("USER:", process.env.MYSQLUSER || process.env.DB_USER);
+console.log("PORT:", process.env.MYSQLPORT || process.env.DB_PORT);
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host:
+    process.env.MYSQLHOST || process.env.DB_HOST || "mysql.railway.internal",
+  user: process.env.MYSQLUSER || process.env.DB_USER || "root",
+  password:
+    process.env.MYSQLPASSWORD ||
+    process.env.DB_PASSWORD ||
+    process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME || "railway",
+  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
   ssl: {
     rejectUnauthorized: false,
   },
