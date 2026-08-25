1a41edfd3a4a13465021299f82ec36763ceb73eb
Halls499 <raul.mazzone@etec.sp.gov.br>
2026-08-24 23:02:31 -0300

    nem sei oq to fznd

--- Back/config/database.js
+++ Back/config/database.js
@@ -3,17 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("🔍 Tentando conectar com senha direta...");
-
-const pool = mysql.createPool({
-  host: "mysql.railway.internal",
-  user: "root",
-  password: "NJmciYzGhdnywaukIdrvTirxMfxobULD", // Senha fixa direto aqui para testar
-  database: "railway",
-  port: 3306,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 export default pool;
