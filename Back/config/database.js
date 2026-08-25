ce8ea914d0fd515743f46aa12687526d1dcb98ec
Halls499 <raul.mazzone@etec.sp.gov.br>
2026-08-24 21:31:13 -0300

    variaveis de ambiente separadas

--- Back/config/database.js
+++ Back/config/database.js
@@ -4,10 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
